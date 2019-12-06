30a891c741558893d44ce483df1daf70e3df5787
Sam Watson <SWatson@spartaglobal.com>
2019-12-06 11:38:27 +0000

    Monster_uni run

---
+++ monster_uni_run.py
@@ -0,0 +1,13 @@
+from monster_uni_classes import *
+
+Sam = Student_Monster(4756, '2:1')
+print(Sam.uni_id)
+print(Sam.grade)
+spooktober = Spooky_Workshop('Scaretime')
+print(spooktober.subject)
+spooktober.add_student(4756)
+spooktober.add_student(7856)
+print('Spooktober subject:', spooktober.subject, '\nStudents:', spooktober.students)
+
+
+
