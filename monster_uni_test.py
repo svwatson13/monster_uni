5a3895fc34755d956aaca809e9781a4e9da338b6
Sam Watson <SWatson@spartaglobal.com>
2019-12-06 11:54:51 +0000

    Cleaner test

--- monster_uni_test.py
+++ monster_uni_test.py
@@ -2,17 +2,21 @@ from monster_uni_classes import *
 from monster_uni_run import *
 
 # Testing creation of uni_id and grade for each student
+print('This is a test of whether we can succesfull add uni id and grade to a student')
 print(Student_Monster(4756, '2:1').uni_id == 4756)
 print(Student_Monster(4756, '2:1').grade == '2:1')
 
 # Testing list of workshops
+print('\nThis is the list of workshops:')
 print(workshops)
 
 # Testing creation of workshop
+print('\nThis is a test of creating a workshop and designating a subject:')
 print(Spooky_Workshop('Scaretime').subject == 'Scaretime')
 Spooky_Workshop('Scaretime')
 
 # Testing enrollment of students to workshops
+'\nThis is a test of adding students to workshops'
 spooktober = Spooky_Workshop('Scaretime')
 spooktober.add_student(4756)
 print(spooktober.students == [4756])
